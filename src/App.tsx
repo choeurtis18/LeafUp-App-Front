49fcc66c61267b4c2f2777aab0cd0ca2ef538b27
Noham <72859612+Noh4m@users.noreply.github.com>
2022-05-17 12:08:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-2xl'>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
