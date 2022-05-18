28c127caa485db4f323d59e4e1ba41b242f74d72
Choeurtis <choeurtis.tchounga@gmail.com>
2022-05-18 14:25:32 +0200

    Creation de la navBar et et des différentes pages

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function NotFound() {
+  return (
+    <div>
+        <NavLink to="/">
+            <span>Retourner à l'ccueil</span>
+        </NavLink>
+    </div>
+  )
+
+}   
---
+++ src/Pages/PlantePage.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+export default function PlantePage() {
+  return (
+    <div>
+      <h1>Plante page</h1>
+    </div>
+  )
+
+}   
