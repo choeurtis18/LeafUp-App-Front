78446802fc033b2b736ac3c09712a72c9c6a1f51
Choeurtis <choeurtis.tchounga@gmail.com>
2022-06-26 23:05:33 +0200

    Navbar clean mobile+desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import Hamburger from 'hamburger-react';
 
 import logo_green from './Asset/logo_green.svg';
 import logo_white from './Asset/logo_white.svg';
@@ -12,46 +11,52 @@ import ParametrePage from './Pages/ParametrePage';
 import PlantePage from './Pages/PlantePage';
 import ProfilPage from './Pages/ProfilPage';
 import TrickAndTipsPage from './Pages/TrickAndTipsPage';
-import { exit } from 'process';
 
 function App() {
-  const [isOpen, setOpen] = useState(true)
-
   const navActive = (event: { target: any; }) => {
-    if(event.target.closest('a')) {  
-      document.querySelectorAll('.navbar li').forEach((element) => {
+    if(event.target.closest('a') && window.matchMedia("(min-width: 481px)")) {  
+      document.querySelectorAll('.desktop-navbar li').forEach((element) => {
         element.classList.remove('active');
       })
       
       var nav_element = event.target.closest('li');
-      nav_element.classList.add('active');
+      if(nav_element) {  
+        nav_element.classList.add('active');
+      }
     }
   }
   document.addEventListener('mousedown', navActive, false);
 
-  const closeNav = (event: { target: any; }) => {
-    if(event.target.closest('.hamburger-react').ariaExpanded === 'true') { 
-      document.querySelectorAll('.navbar li').forEach((element) => {
-        element.classList.add('hide');
-      })
-    } else {
-      document.querySelectorAll('.navbar li').forEach((element) => {
-        element.classList.remove('hide');
-      })
-    }
-  }
-  document.addEventListener('mousedown', closeNav, false);
-
   return (
     <div className="App">
       <BrowserRouter>      
-        <nav className="navbar">
-          <Hamburger toggled={isOpen} toggle={setOpen} size={20} />
-          <li><img className="img_green" src={logo_green} /><img className="img_white" src={logo_white} /><NavLink to="" className="nav-link">Accueil</NavLink></li>
-          <li><img className="img_green" src={logo_green} /><img className="img_white" src={logo_white} /><NavLink to="/maplante" className="nav-link">Ma plante</NavLink></li>
-          <li><img className="img_green" src={logo_green} /><img className="img_white" src={logo_white} /><NavLink to="/tricksandtips" className="nav-link">Tricks and Tips</NavLink></li>
-          <li><img className="img_green" src={logo_green} /><img className="img_white" src={logo_white} /><NavLink to="/profil" className="nav-link">Mon Profil</NavLink></li>
-          <li><img className="img_green" src={logo_green} /><img className="img_white" src={logo_white} /><NavLink to="/parametre" className="nav-link">Parametre</NavLink></li>	
+        <nav className="desktop-navbar">
+          <a className="desktop-navbar-closebtn">&times;</a>
+          <li>
+            <img className="img_logo_green" src={logo_green} />
+            <img className="img_logo_white" src={logo_white} />
+            <NavLink to="" className="desktop-nav-link">Accueil</NavLink>
+          </li>
+          <li>
+            <img className="img_logo_green" src={logo_green} />
+            <img className="img_logo_white" src={logo_white} />
+            <NavLink to="/maplante" className="desktop-nav-link">Ma plante</NavLink>
+          </li>
+          <li>
+            <img className="img_logo_green" src={logo_green} />
+            <img className="img_logo_white" src={logo_white} />
+            <NavLink to="/tricksandtips" className="desktop-nav-link">Tricks and Tips</NavLink>
+          </li>
+          <li>
+            <img className="img_logo_green" src={logo_green} />
+            <img className="img_logo_white" src={logo_white} />
+            <NavLink to="/profil" className="desktop-nav-link">Mon Profil</NavLink>
+          </li>
+          <li>
+            <img className="img_logo_green" src={logo_green} />
+            <img className="img_logo_white" src={logo_white} />
+            <NavLink to="/parametre" className="desktop-nav-link">Parametre</NavLink>
+          </li>	
         </nav>
 
         <Routes>
@@ -63,7 +68,6 @@ function App() {
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+//Icon
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 import logo_green from './Asset/logo_green.svg';
+//npm i --save @fortawesome/react-fontawesome@latest
 
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_overlay
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
+const openMenu = (event: { target: any; }) => {
+  if(event.target.closest('.header-container-menu-icon')) {  
+    var nav_bar = event.target.closest('#root').querySelector('.desktop-navbar');
+    nav_bar.classList.add('mobile');
+  }
+}
+document.addEventListener('mousedown', openMenu, false);
+
+const closeMenu = (event: { target: any; }) => {
+  if(event.target.closest('.desktop-navbar-closebtn')) {  
+    var nav_bar = event.target.closest('.desktop-navbar');
+    nav_bar.classList.remove('mobile');
+  }
+}
+document.addEventListener('mousedown', closeMenu, false);
+
 root.render(
   <React.StrictMode>
   <header>
-    <p>Leaf'UP</p>
-    <img src={logo_green}/>
+    <div className='header-logo-container'>
+      <p>Leaf'UP</p>
+      <img src={logo_green}/>
+    </div>
+    <div className='header-container'>
+      <FontAwesomeIcon icon={faSignOutAlt} className='header-container-sign-icon' />
+      <FontAwesomeIcon icon={faBars} className='header-container-menu-icon'/>
+    </div>
   </header>
     <App />
   </React.StrictMode>
