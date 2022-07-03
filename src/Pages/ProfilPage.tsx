e6b3e0d3ff7994d7baa44d298a3ab5642d33674d
Melvin Tom-A-Kien <melvin.tom-a-kien@hotmail.com>
2022-07-03 16:38:31 +0200

    profil page

--- src/Pages/ProfilPage.tsx
+++ src/Pages/ProfilPage.tsx
@@ -1,9 +1,64 @@
 import React, { useState, useEffect } from 'react';
+import Header from '../Header';
+import Posts from '../Posts';
+import profil from '../Asset/jennifer.jpeg';
+import iconpost from '../Asset/iconpost.png'
+import likepost from '../Asset/likepost.png'
+import background from "../Asset/backgroundHeader.jpg";
 
 export default function ProfilPage() {
+  
+  for(let i = 0; i < 5; ++i){
+      
+  }
   return (
     <div className='container'>
-      <h1>Profil page</h1>
+      <Header
+       IMGprofil = {profil}
+       cover = {background}
+       full_name = "Jennifer Lawrence"
+       plant_name = "FITTONIA"
+       description = "United States"
+      />
+      <div className="flex justify-center gap-8 mb-4">
+        <div className="w-1/2 lg:w-1/4 bg-white lg:px-24 py-2 shadow-md">
+          <img className="mx-auto" src={iconpost} alt={iconpost} />
+        </div>
+        <div className="w-1/2 lg:w-1/4 lg:px-24 py-2">
+          <img className="mx-auto" src={likepost} alt={likepost} />
+        </div>
+      </div>
+      <div className="grid lg:grid-cols-3 gap-4">
+        <Posts 
+          post_title = "Comment prendre soin de ses plantes ?"
+          post_content = "Dans ce Post vous trouverez tous les conseils nécessaires pour vous aider à garder vos plantes en vie. "
+          post_author = "J. Lawrence"
+          post_url = "post/11"
+        />
+                <Posts 
+          post_title = "Comment prendre soin de ses plantes ?"
+          post_content = "Dans ce Post vous trouverez tous les conseils nécessaires pour vous aider à garder vos plantes en vie. "
+          post_author = "J. Lawrence"
+          post_url = "post/11"
+        />
+        
+        <Posts 
+          post_title = "Comment prendre soin de ses plantes ?"
+          post_content = "Dans ce Post vous trouverez tous les conseils nécessaires pour vous aider à garder vos plantes en vie. "
+          post_author = "J. Lawrence"
+          post_url = "post/11"
+        />
+        
+        <Posts 
+          post_title = "Comment prendre soin de ses plantes ?"
+          post_content = "Dans ce Post vous trouverez tous les conseils nécessaires pour vous aider à garder vos plantes en vie. "
+          post_author = "J. Lawrence"
+          post_url = "post/11"
+        />
+        
+        
+      </div>
+      
     </div>
   )
 
---
+++ src/Posts.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Posts(props : any) {
+ 
+  return (
+    <div className=" bg-white w-4/5 rounded-md mt-5 relative p-5 mx-auto">
+        <h2 className="font-semibold text-md">{props.post_title}</h2>
+        <p className="font-medium text-[#3b4a34]">{props.post_content}</p>
+        <p className="text-right font-bold mb-3">{props.post_author}</p>
+        <a  href={props.post_url}>
+            <button className="py-2 px-6 bg-[#6e8464] text-white rounded-3xl cursor-pointer">
+                Lire le Post
+            </button>
+        </a>
+    </div>
+  )
+}
