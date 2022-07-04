6cce45c6f19154e0ababb9286eede31a06843fe7
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2022-07-04 15:19:32 +0200

    Ajout pages TricksAndTips et Post

---
+++ src/datas/PostList.tsx
@@ -0,0 +1,12 @@
+export const postList = [
+    {
+        id: 1,
+        titre: 'La vie est belle',
+        description: 'Deuxième album, deuxième disque, la pression, le soutien du quartier Des doutes et des doutes et des doutes, j hésite à faire du sale ou bien chanter ',
+        nom : 'Damso',
+        prenom : 'Belge',
+        niveau : 'Debutants',
+        date : 12/10/2019
+    }
+]
+
