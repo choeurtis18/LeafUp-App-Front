317f1ed00e1a7b4c3a01c4cc46f52bc693bcd420
Noham <72859612+Noh4m@users.noreply.github.com>
2022-07-05 12:38:22 +0200

    home versin desktop v1

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react';
 
 function Card(props : any) {
     return (
-        <div className={'rounded-xl h-52 relative mt-6 ' + props.height +' '+ props.shadow}>
+        <div className={'rounded-xl h-52 relative mt-6 lg:mb-12' + props.css }>
             {props.children}
         </div>
     );
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -5,15 +5,15 @@ import Card from '../Components/Card';
 export default function HomePage() {
   return (
     <div className='container mx-auto px-4'>
-         <Card> 
-          <img src={GifPlant} alt="GifPlant" className='rounded-xl'/>
+         <Card css=' lg:h-80 '> 
+          <img src={GifPlant} alt="GifPlant" className='rounded-xl lg:h-full lg:w-full lg:object-cover'/>
             <div className='text-white p-8 bottom-0 absolute'>
               <p>Leaf Up</p>
               <p>Prenez soin de vos plantes</p>
             </div>
          </Card>
 
-         <Card height='h-64' shadow='shadow-2xl'> 
+         <Card css='h-64 shadow-2xl w-1/2'> 
             <div className='text-black p-8 bottom-0 absolute'>
               <p className='text-base'>Découvrez notre nouvelle serre connectée</p>
               <p className='text-xs mb-6'>A la pointe de la technologie, cette serre vous permettra de prendre soin de votre plante en vous indiquant les différents  </p>
@@ -23,6 +23,7 @@ export default function HomePage() {
               </div>
             </div>
          </Card>
+         
     </div>
   )
 
