fe62a0e74f004455d792832343739ac661d5f4b3
Choeurtis <choeurtis.tchounga@gmail.com>
2022-07-08 00:24:34 +0200

    Add set post and set comment Hook

---
+++ src/Hook/useSetPost.tsx
@@ -0,0 +1,9 @@
+import {TrickAndTipsData} from "../interfaces/TrickAndTips";
+import axios from "axios";
+
+export default function useSetPost(user_id:number) {
+    return (): Promise<TrickAndTipsData> => {
+        return axios.get('http://127.0.0.1:8000/api/posts/user_id='+{user_id})
+            .then(res => res.data)
+    }
+}
