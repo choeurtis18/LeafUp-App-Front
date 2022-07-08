b295f17da17f8edded37629db85fcf4301efa9e5
Choeurtis <choeurtis.tchounga@gmail.com>
2022-07-08 15:00:59 +0200

    debug #1

--- src/Hook/useGetPostComments.tsx
+++ src/Hook/useGetPostComments.tsx
@@ -2,6 +2,8 @@ import {Comments} from "../interfaces/Comments"
 import axios from "axios";
 
 export default function useGetPostComments(post_id:number) {
+    console.log("In useGetPostComments");
+    console.log(post_id);
     return (): Promise<Comments[]> => {
         return axios.get('http://127.0.0.1:8000/api/commentbypost/'+{post_id})
             .then(res => res.data)
