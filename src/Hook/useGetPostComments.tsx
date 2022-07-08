e4879a1ed5bca173bc4480b67b44de27c0662234
Choeurtis <choeurtis.tchounga@gmail.com>
2022-07-08 15:06:01 +0200

    debug #2

--- src/Hook/useGetPostComments.tsx
+++ src/Hook/useGetPostComments.tsx
@@ -3,9 +3,11 @@ import axios from "axios";
 
 export default function useGetPostComments(post_id:number) {
     console.log("In useGetPostComments");
-    console.log(post_id);
+    var id = String(post_id);
+    console.log('http://127.0.0.1:8000/api/commentbypost/'+{id});
+
     return (): Promise<Comments[]> => {
-        return axios.get('http://127.0.0.1:8000/api/commentbypost/'+{post_id})
+        return axios.get('http://127.0.0.1:8000/api/commentbypost/'+{id})
             .then(res => res.data)
     }
 }
